8b8349b9b647a7c98d9c6bc13f0ade90906e341f
Helloyunho <yunho050840@gmail.com>
2020-05-10 10:22:30 +0900

    Update to use cache

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const Koa = require('koa')
-const koaStatic = require('koa-static')
+const koaStatic = require('koa-static-cache')
 const mount = require('koa-mount')
 const send = require('koa-send')
 const path = require('path')
 
 const app = new Koa()
 
-app.use(async ctx => {
-  ctx.set('Cache-Control', 'max-age=86400')
-})
-app.use(mount('/', koaStatic('build/')))
+app.use(
+  mount(
+    '/',
+    koaStatic('build/', {
+      maxAge: 24 * 60 * 60
+    })
+  )
+)
 app.use(async ctx => {
   if (ctx.status === 404) {
     await send(ctx, 'index.html', {
