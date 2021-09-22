503be39a60b4d2334843186c0aded20b38144406
Helloyunho <yunho050840@gmail.com>
2021-09-23 03:27:22 +0900

    ➖ chore: remove unnecessary deps

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,22 +3,8 @@ module.exports = {
     public: '/',
     src: '/_dist_'
   },
-  plugins: [
-    '@snowpack/plugin-react-refresh',
-    // [
-    //   'snowpack-plugin-import-map',
-    //   {
-    //     imports: {
-    //       '*': true
-    //     },
-    //     dev: false
-    //   }
-    // ],
-    '@snowpack/plugin-postcss'
-  ],
+  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-postcss'],
   devOptions: {
     port: 3000
   }
 }
-
-console.log('Fcuk')
